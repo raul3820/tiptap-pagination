87ae7be5c591c711802d5b5cacc59e84838d1a84
Romik <40670677+RomikMakavana@users.noreply.github.com>
2025-05-21 12:31:16 +0530

    Enhance project with new OpenForWork component, update button styles, and add 404 error page. Update package dependencies and scripts in package.json for improved functionality.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { OpenForWork } from "@/ui/open-for-work";
 import TiptapEditor from "@/ui/tiptap-editor";
 
 function Home() {
   return (
     <div className="max-w-4xl mx-auto px-2 relative">
       <TiptapEditor />
+      <OpenForWork />
     </div>
   );
 }
---
+++ src/ui/open-for-work.tsx
@@ -0,0 +1,31 @@
+import { Button } from "./button";
+
+export const OpenForWork = () => {
+  return (
+    <div
+      className="fixed bottom-0 border rounded-lg shadow-sm shadow-amber-400 p-2 bg-muted/90 backdrop-blur-md z-50 mb-4"
+      style={{
+        right: "50vw",
+        marginLeft: "auto",
+        alignSelf: "center",
+        transform: "translateX(50%)",
+      }}
+    >
+      <div className="flex flex-row items-center justify-between gap-2">
+        <div>
+          <h1 className="text-sm">
+            {" "}
+            I'm <span className="font-bold">Open for Work</span>
+          </h1>
+          <p className="text-xs">Let's talk about your project</p>
+
+        </div>
+        <a href="https://forms.gle/KGVAhKSUnjFnGaAw8" target="_blank" rel="noopener noreferrer">
+          <Button variant="outline" className="!bg-amber-400 text-black hover:text-amber-50 !transition-all duration-150">
+            Contact Me
+          </Button>
+        </a>
+      </div>
+    </div>
+  );
+};
